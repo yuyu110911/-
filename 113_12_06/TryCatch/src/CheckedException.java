0a62e1a1ab2b2330d1ff083a56a89e7531a6258e
yuyu110911 <A112223009@mail.shu.edu.tw>
2024-12-13 10:28:17 +0800

    Update CheckedException.java

--- 113_12_06/TryCatch/src/CheckedException.java
+++ 113_12_06/TryCatch/src/CheckedException.java
@@ -1,8 +1,17 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import  java.io.FileReader;
 
 
 public class CheckedException {
+    public static void main(String[] args){
+        try {
+            File file = new File("nonexistentfile.txt");
+            FileReader reader = new FileReader(file);
+        }catch (FileNotFoundException e){
+            System.out.println("處理受檢例外:" + e.getMessage());
+        }
+    }
 
 }
 
